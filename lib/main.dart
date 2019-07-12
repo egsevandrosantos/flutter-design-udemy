91d5d5e7279299bbf6787d1a6ab5229a1d6ef087
Evandro Gabriel dos Santos <egs.evandro.santos@gmail.com>
2019-07-12 13:40:42 -0300

    Padding e Sizebox

--- lib/main.dart
+++ lib/main.dart
@@ -21,25 +21,58 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        body: MyAppWidget(),
+        body: Container(
+          width: double.infinity,
+          color: Colors.yellow,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              ButtonIcon(),
+              ButtonText(),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
 
-class MyAppWidget extends StatelessWidget {
+class ButtonIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 30),
       child: Container(
-        width: 300,
+        width: double.infinity,
         height: 50,
         color: Colors.red,
-        alignment: Alignment.center,
         child: Icon(
-          Icons.add_a_photo,
+          Icons.add,
           color: Colors.white,
-        )
+        ),
+      ),
+    );
+  }
+}
+
+class ButtonText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 30),
+      child: Container(
+        width: double.infinity,
+        height: 50,
+        color: Colors.blue,
+        alignment: Alignment.center,
+        child: Text(
+          "Toque aqui",
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 18
+          ),
+        ),
       ),
     );
   }
